357c8210f60f66693f36f2b79a21363b68c73dff
Sly321 <liebigsv@gmail.com>
2017-09-01 00:31:16 +0200

    router comments

--- src/router.tsx
+++ src/router.tsx
@@ -7,10 +7,34 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import App from './views/app/App';
 import Error404 from './views/error/404/Error404';
 
+// Should i create a class for this?
+interface RouteInterface {
+	path: string;
+	component: React.ComponentClass;
+	key: string;
+}
+
+// Add routes here
+const routesArray: Array<RouteInterface> = [
+	{ path: '/', 	component: App, key: '1' },
+	{ path: '/2', 	component: App, key: '2' }
+];
+
+/**
+ * Iterate through the routesArray and generate a Route Element for
+ * each entry of {@link RouteInterface}.
+ */
+const routesElements = routesArray.map((route: RouteInterface) =>
+	<Route exact key={ route.key } path={ route.path } component={ route.component } /> 
+);
+
+/**
+ * Generates routes, last route is a 404 notfound.
+ */
 const Routes = () => (
 	<Router>
 		<Switch>
-			<Route exact path="/" component={ App }/>
+			{ routesElements }
 			<Route component={ Error404 }/>
 		</Switch>
 	</Router>
