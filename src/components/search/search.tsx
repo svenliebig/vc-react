e993dd2881326f5a99535b3df43df2d807727641
Sly321 <liebigsv@gmail.com>
2017-09-06 23:43:47 +0200

    implement a wait function for the search input

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,6 +1,9 @@
 /** Import React */
 import * as React from 'react';
 
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/debounceTime';
+
 /** Stylesheet Imports */
 import './search.css';
 
@@ -13,7 +16,6 @@ interface Props {
 
 interface State {
 	inputClass: string;
-	value: string;
 }
 
 class Search extends React.Component<Props> {
@@ -21,19 +23,36 @@ class Search extends React.Component<Props> {
 	public placeholder = 'Suche...';
 	public inputClass = 'input-danger';
 
+	private searchStringObs: Subject<string>;
+
+	private INPUT_DANGER = 'input-danger';
+	private INPUT_PROGESS = 'input-progress';
+	
 	constructor(public props: Props) {
 		super(props);
-
-		this.onChange.bind(this);
+		
+		this.onChange = this.onChange.bind(this);
+		this.method   = this.method.bind(this);
+		
+		this.searchStringObs = new Subject<string>();
+		this.searchStringObs.debounceTime(500).subscribe(this.method);
 
 		this.state = {
-			inputClass: '',
-			value: ''
+			inputClass: ''
 		};
 	}
 
+	method(val: string): void {
+		this.setState(() => ({
+			inputClass: this.INPUT_DANGER
+		}));
+	}
+
 	onChange(evt: React.ChangeEvent<HTMLInputElement>): void {
-		// evt.target.value;
+		this.setState(() => ({
+			inputClass: this.INPUT_PROGESS
+		}));
+		this.searchStringObs.next(evt.target.value);
 	}
 
 	render() {
